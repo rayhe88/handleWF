c397d7dc5f39f8ea8f2c40b4274456af884cdc20
RayHE <rayhe88@gmail.com>
2024-03-06 01:31:31 -0600

    Add src file with source code

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "Wavefunction.hpp"
+#include "version.hpp"
+
+int main(int argc, char *argv[]) {
+  std::cout << "Version: " << PROJECT_VER << std::endl;
+  std::cout << "Compilation Date: " << __DATE__ << "  " << __TIME__ << std::endl;
+ std::cout << "Git SHA1: " << GIT_SHA1 << std::endl;
+
+  Wavefunction wf;
+  wf.loadWF(argv[1]);
+
+  wf.printWF();
+}
